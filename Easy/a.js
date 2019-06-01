9b40c430dda2111a98a1dfad2cc6dde13862c4bd
Jack Weisse <jackweisse@yahoo.com>
2019-06-01 14:24:41 -0500

    adding more

--- Easy/a.js
+++ Easy/a.js
@@ -4,3 +4,40 @@
 
 // Bonus: Can you do this in one pass?
 
+const testFunction = require('../utils/testFunction')
+
+const sumChecker = (numberList, expectedSum) => {
+	const subChecker = (sum, starter, numList) => {
+		if(!numList.length) {
+			return false
+		}
+
+		if(sum === starter + numList[0]) {
+			return true
+		}
+
+		return subChecker(sum, starter, numList.slice(1))
+	}
+
+	let results = false
+	let i = 0
+
+	while(!results && i < numberList.length-1) {
+		results = subChecker(expectedSum, numberList[i], numberList.slice(i+1))
+		i++
+	}
+
+	return results
+}
+
+const testCases = [
+	[[10, 15, 3, 7], 17, true],
+	[[0, 1, 2, 3, 4, 5], 30, false],
+	[[1, 1, 1, 1, 1], 2, true],
+	[[0, 1, 2, 3, 4], 7, true],
+	[[], 0, false],
+	[[1], 1, false],
+	[[17, -2], 15, true]
+]
+
+testFunction(sumChecker, testCases)
